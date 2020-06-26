754013d29a848e766eb1bce237cb372be26fb6e3
Pavel Tokarev <tokarevpavel1996@mail.ru>
2020-06-26 13:00:15 +0300

    Final version

--- plot_picture_Rspec_MM.m
+++ plot_picture_Rspec_MM.m
@@ -1,7 +1,8 @@
 clc; clear;%close all
 Eb_N0 = 0:40;
+%% ЧБ
 %% beamfrming и передача копий
-figure()
+figure(1)
 load('DataBase/RAYL_SPECIAL/4x4x4_pre =0_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
 mean_bear = mean(ber_mean,1);
 plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'k',1.5,0);
@@ -21,7 +22,7 @@ legend('4x4x4','8x4x4','16x4x4','32x4x4','64x4x4');
 ylim([10^-5 10^0]);
 xlim([0 40]);
 %% разнесенный прием
-figure()
+figure(2)
 load('DataBase/RAYL_SPECIAL/4x4x4_pre =0_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
 mean_bear = mean(ber_mean,1);
 plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'k',1.5,0);
@@ -41,7 +42,7 @@ legend('4x4x4','4x8x4','4x16x4','4x32x4','4x64x4');
 ylim([10^-5 10^0]);
 xlim([0 40]);
 %% симметричные антенны
-figure()
+figure(3)
 load('DataBase/RAYL_SPECIAL/4x4x4_pre =0_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
 mean_bear = mean(ber_mean,1);
 plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'k',1.5,0);
@@ -54,6 +55,64 @@ plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'-.k',1.5,0);
 load('DataBase/RAYL_SPECIAL/32x32x4_pre =1_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
 mean_bear = mean(ber_mean,1);
 plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,':k',1.5,0);
+legend('4x4x4','8x8x4','16x16x4','32x32x4');
+ylim([10^-5 10^0]);
+xlim([0 40]);
+%% ЦВЕТ
+%% beamfrming и передача копий
+figure(4)
+load('DataBase/RAYL_SPECIAL/4x4x4_pre =0_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'k',1.5,0);
+load('DataBase/RAYL_SPECIAL/8x4x4_pre =1_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'r',1.5,0);
+load('DataBase/RAYL_SPECIAL/16x4x4_pre =1_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'b',1.5,0);
+load('DataBase/RAYL_SPECIAL/32x4x4_pre =1_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'g',1.5,0);
+load('DataBase/RAYL_SPECIAL/64x4x4_pre =1_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'c',1.5,0);
+legend('4x4x4','8x4x4','16x4x4','32x4x4','64x4x4');
+ylim([10^-5 10^0]);
+xlim([0 40]);
+%% разнесенный прием
+figure(5)
+load('DataBase/RAYL_SPECIAL/4x4x4_pre =0_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'k',1.5,0);
+load('DataBase/RAYL_SPECIAL/4x8x4_pre =1_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'r',1.5,0);
+load('DataBase/RAYL_SPECIAL/4x16x4_pre =1_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'b',1.5,0);
+load('DataBase/RAYL_SPECIAL/4x32x4_pre =1_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'g',1.5,0);
+load('DataBase/RAYL_SPECIAL/4x64x4_pre =1_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'c',1.5,0);
+legend('4x4x4','4x8x4','4x16x4','4x32x4','4x64x4');
+ylim([10^-5 10^0]);
+xlim([0 40]);
+%% симметричные антенны
+figure(6)
+load('DataBase/RAYL_SPECIAL/4x4x4_pre =0_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'k',1.5,0);
+load('DataBase/RAYL_SPECIAL/8x8x4_pre =1_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'r',1.5,0);
+load('DataBase/RAYL_SPECIAL/16x16x4_pre =1_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'b',1.5,0);
+load('DataBase/RAYL_SPECIAL/32x32x4_pre =1_ster =0_RAYL_SPECIAL_Wmm=0_Wm=0_Exp=1.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'g',1.5,0);
 legend('4x4x4','8x8x4','16x16x4','32x32x4');
 ylim([10^-5 10^0]);
 xlim([0 40]);
--- plot_picture_rayl_MM.m
+++ plot_picture_rayl_MM.m
@@ -1,7 +1,8 @@
 clc; clear;%close all
 Eb_N0 = 0:40;
+%% ЧБ
 %% beamfrming и передача копий
-figure()
+figure(1)
 load('DataBase/RAYL/4x4x4_pre =0_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
 mean_bear = mean(ber_mean,1);
 plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'k',1.5,0);
@@ -18,7 +19,7 @@ legend('4x4x4','8x4x4','16x4x4','32x4x4');
 ylim([10^-5 10^0]);
 xlim([0 40]);
 %% разнесенный прием
-figure()
+figure(2)
 load('DataBase/RAYL/4x4x4_pre =0_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
 mean_bear = mean(ber_mean,1);
 plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'k',1.5,0);
@@ -35,7 +36,7 @@ legend('4x8x4','4x8x4','4x16x4','4x32x4');
 ylim([10^-5 10^0]);
 xlim([0 40]);
 %% симметричные антенны
-figure()
+figure(3)
 load('DataBase/RAYL/4x4x4_pre =0_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
 mean_bear = mean(ber_mean,1);
 plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'k',1.5,0);
@@ -48,6 +49,58 @@ plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'-.k',1.5,0);
 load('DataBase/RAYL/32x32x4_pre =1_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
 mean_bear = mean(ber_mean,1);
 plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,':k',1.5,0);
+legend('4x4x4','8x8x4','16x16x4','32x32x4');
+ylim([10^-5 10^0]);
+xlim([0 40]);
+%% ЦВЕТ
+%% beamfrming и передача копий
+figure(4)
+load('DataBase/RAYL/4x4x4_pre =0_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'k',1.5,0);
+load('DataBase/RAYL/8x4x4_pre =1_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'r',1.5,0);
+load('DataBase/RAYL/16x4x4_pre =1_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'b',1.5,0);
+load('DataBase/RAYL/32x4x4_pre =1_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'g',1.5,0);
+legend('4x4x4','8x4x4','16x4x4','32x4x4');
+ylim([10^-5 10^0]);
+xlim([0 40]);
+%% разнесенный прием
+figure(5)
+load('DataBase/RAYL/4x4x4_pre =0_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'k',1.5,0);
+load('DataBase/RAYL/4x8x4_pre =1_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'r',1.5,0);
+load('DataBase/RAYL/4x16x4_pre =1_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'b',1.5,0);
+load('DataBase/RAYL/4x32x4_pre =1_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'g',1.5,0);
+legend('4x8x4','4x8x4','4x16x4','4x32x4');
+ylim([10^-5 10^0]);
+xlim([0 40]);
+%% симметричные антенны
+figure(6)
+load('DataBase/RAYL/4x4x4_pre =0_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'k',1.5,0);
+load('DataBase/RAYL/8x8x4_pre =1_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'r',1.5,0);
+load('DataBase/RAYL/16x16x4_pre =1_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'b',1.5,0);
+load('DataBase/RAYL/32x32x4_pre =1_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
+mean_bear = mean(ber_mean,1);
+plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'g',1.5,0);
 legend('4x4x4','8x8x4','16x16x4','32x32x4');
 ylim([10^-5 10^0]);
 xlim([0 40]);

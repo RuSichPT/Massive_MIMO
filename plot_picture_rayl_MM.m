39a26b4c1116c0f050d9452c67035af495d817d0
Pavel Tokarev <tokarevpavel1996@mail.ru>
2020-06-26 13:57:08 +0300

    Final version

--- plot_picture_rayl_MM.m
+++ plot_picture_rayl_MM.m
@@ -32,7 +32,7 @@ plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'-.k',1.5,0);
 load('DataBase/RAYL/4x32x4_pre =1_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
 mean_bear = mean(ber_mean,1);
 plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,':k',1.5,0);
-legend('4x8x4','4x8x4','4x16x4','4x32x4');
+legend('4x4x4','4x8x4','4x16x4','4x32x4');
 ylim([10^-5 10^0]);
 xlim([0 40]);
 %% симметричные антенны
@@ -84,7 +84,7 @@ plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'b',1.5,0);
 load('DataBase/RAYL/4x32x4_pre =1_ster =0_RAYL_Wmm=0_Wm=0_Exp=50.mat');
 mean_bear = mean(ber_mean,1);
 plot_ber(mean_bear,SNR,prm.bps*prm.numSTS,'g',1.5,0);
-legend('4x8x4','4x8x4','4x16x4','4x32x4');
+legend('4x4x4','4x8x4','4x16x4','4x32x4');
 ylim([10^-5 10^0]);
 xlim([0 40]);
 %% симметричные антенны
